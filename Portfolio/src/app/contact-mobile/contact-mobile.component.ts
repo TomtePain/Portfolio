ac17509eedc939792ee263958db337f2e41b818f
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-06-21 13:32:38 +0200

    some bugfixes

--- Portfolio/src/app/contact-mobile/contact-mobile.component.ts
+++ Portfolio/src/app/contact-mobile/contact-mobile.component.ts
@@ -1,16 +1,45 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact-mobile',
   templateUrl: './contact-mobile.component.html',
   styleUrls: ['./contact-mobile.component.scss']
 })
-export class ContactMobileComponent {
+export class ContactMobileComponent implements OnInit {
+  ngOnInit(): void {
+  }
+
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
   @ViewChild('mailField') mailField!: ElementRef;
   @ViewChild('messageField') messageField!: ElementRef;
   @ViewChild('sendButton') sendButton!: ElementRef;
+
+  nameFilled: boolean = false;
+  mailFilled: boolean = false;
+  messageFilled: boolean = false;
+
+  checkFields() {
+    let nameField: HTMLElement | any = document.getElementById('nameField');
+    let mailField: HTMLElement | any = document.getElementById('mailField');
+    let messageField: HTMLElement | any = document.getElementById('msg');
+
+    if (nameField.value != '') {
+      this.nameFilled = true;
+    } else {
+      this.nameFilled = false;
+    }
+
+    if (mailField.value != '') {
+        this.mailFilled = true
+    } else { this.mailFilled = false }
+
+    if(messageField.value != '') {
+      this.messageFilled = true 
+    } else {
+      this.messageFilled = false;
+    }
+  }
 
   async sendMail() {
     let nameField = this.nameField.nativeElement;
@@ -60,6 +89,8 @@ export class ContactMobileComponent {
       messageField.disabled = false,
       sendButton.disabled = false
   }
+
+
 
 
 
