030985fc347296a3be49e32eef96a9533f519862
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-06-26 20:12:48 +0200

    added scroll to top for imprint route

--- Portfolio/src/app/imprint/imprint.component.ts
+++ Portfolio/src/app/imprint/imprint.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
   styleUrls: ['./imprint.component.scss']
 })
-export class ImprintComponent {
+export class ImprintComponent implements OnInit {
+
+
+  ngOnInit(): void {
+    this.newWay();
+  }
+
+  newWay() {
+    let anchor: any = document.getElementById('imprint');
+    
+    anchor.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
+  }
 
 }
+
+
