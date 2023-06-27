5c9f858c939fbf14ff11312c5bd49675ca93cb4c
TomtePain <123365386+TomtePain@users.noreply.github.com>
2023-06-27 10:24:16 +0200

    add skill-icons animation and clear some bugs

--- Portfolio/src/app/skills-mobile/skills-mobile.component.ts
+++ Portfolio/src/app/skills-mobile/skills-mobile.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills-mobile',
   templateUrl: './skills-mobile.component.html',
   styleUrls: ['./skills-mobile.component.scss']
 })
-export class SkillsMobileComponent {
+export class SkillsMobileComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.animation();
+  }
+
+
+
+
+  animation() {
+
+    let skills:any = document.querySelector('.skills-desktop');
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const icon:any = entry.target.querySelector('.skill-icons-desktop');
+
+        if (entry.isIntersecting) {
+          icon.classList.add('icon-lines-desktop-animation');
+          return; // if we added the class, exit the function
+        }
+
+        // We're not intersecting, so remove the class!
+        icon.classList.remove('icon-lines-desktop-animation');
+      });
+    });
+
+    observer.observe(skills);
+  }
 }
