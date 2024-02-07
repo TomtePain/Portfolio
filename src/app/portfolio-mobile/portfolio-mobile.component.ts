9a65811a3f10a901df57e02c918ac4971b03b106
Tom Pannier <123365386+TomtePain@users.noreply.github.com>
2024-02-07 10:12:47 +0100

    changing and adding new project

--- src/app/portfolio-mobile/portfolio-mobile.component.ts
+++ src/app/portfolio-mobile/portfolio-mobile.component.ts
@@ -18,28 +18,31 @@ export class PortfolioMobileComponent implements OnInit {
         let firstLeft:any = entry.target.querySelector('.first-left');
         let firstRight:any = entry.target.querySelector('.first-right');
         let secondRight:any = entry.target.querySelector('.second-right');
+        let secondLeft:any = entry.target.querySelector('.second-left');
 
         if (entry.isIntersecting) {
-          this.addClass(firstLeft, firstRight, secondRight)
+          this.addClass(firstLeft, firstRight, secondRight, secondLeft)
           return; 
         }
-        this.removeClass(firstLeft, firstRight, secondRight);
+        this.removeClass(firstLeft, firstRight, secondRight, secondLeft);
       });
     });
     observer.observe(anchor);
   }
 
 
-  addClass(firstLeft:any, firstRight:any, secondRight:any) {
+  addClass(firstLeft:any, firstRight:any, secondRight:any, secondLeft:any) {
     return firstLeft.classList.add('slide-in-left'),
     firstRight.classList.add('slide-in-right'),
-    secondRight.classList.add('slide-in-right');
+    secondRight.classList.add('slide-in-right'),
+    secondLeft.classList.add('slide-in-left');
   }
 
-  removeClass(firstLeft:any, firstRight:any, secondRight:any) {
+  removeClass(firstLeft:any, firstRight:any, secondRight:any, secondLeft:any) {
     return firstLeft.classList.remove('slide-in-left'),
     firstRight.classList.remove('slide-in-right'),
-    secondRight.classList.remove('slide-in-right');
+    secondRight.classList.remove('slide-in-right'),
+    secondLeft.classList.remove('slide-in-left');
   }
 
 }
